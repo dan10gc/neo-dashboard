471fad98190f020ef82da97caf739ac3c3447ad6
Daniel Gonzalez <dan@danielgc.design>
2025-10-06 21:51:48 -0700

    implement NEO data fetching with React Query and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useNeoDataQuery } from "./hooks/useNeoNasaQuery";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, isLoading, error } = useNeoDataQuery();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  // TODO: Use data.near_earth_objects
+  console.log(data);
+
+  return <div>Dashboard</div>;
 }
 
-export default App
+export default App;
---
+++ src/hooks/useNeoNasaQuery.ts
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+
+const NASA_API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+/**
+ *
+ * @returns An object containing startDate and endDate in YYYY-MM-DD format
+ */
+const getDateRange = () => {
+  const today = new Date();
+  const priorDate = new Date().setDate(today.getDate() - 7);
+
+  const startDate = new Date(priorDate).toISOString().split("T")[0];
+  const endDate = today.toISOString().split("T")[0];
+
+  return { startDate, endDate };
+};
+
+/**
+ *
+ * @returns Fetches NEO data from NASA API for the past 7 days
+ */
+const fetchNeoData = async () => {
+  const { startDate, endDate } = getDateRange();
+
+  const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${NASA_API_KEY}`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  return response.json();
+};
+
+/**
+ *
+ * @returns A React Query hook to fetch NEO data
+ */
+export const useNeoDataQuery = () => {
+  return useQuery({
+    queryKey: ["neoData"],
+    queryFn: fetchNeoData,
+    staleTime: 1000 * 60 * 5, // Data fresh for 5 minutes
+  });
+};
